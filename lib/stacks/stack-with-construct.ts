bea2e76a026d10cf870def305135cdc297961962
C. Spencer Beggs <spencer@beg.gs>
2020-04-07 15:33:05 -0400

    Prettier 2.0 formatting

--- lib/stacks/stack-with-construct.ts
+++ lib/stacks/stack-with-construct.ts
@@ -4,10 +4,10 @@ import { MyConstruct } from "../constructs/imported-construct";
 
 // Your exported class must be named TargetStack
 export class TargetStack extends Stack {
-    public readonly construct: MyConstruct;
-    public constructor(scope: App, id: string, props?: StackProps) {
-        super(scope, id, props);
-        // write your code here
-        this.construct = new MyConstruct(this, "MyConstruct", {});
-    }
+	public readonly construct: MyConstruct;
+	public constructor(scope: App, id: string, props?: StackProps) {
+		super(scope, id, props);
+		// write your code here
+		this.construct = new MyConstruct(this, "MyConstruct", {});
+	}
 }
--- src/stacks/poly-bucket.ts
+++ src/stacks/poly-bucket.ts
@@ -4,13 +4,13 @@ import { Bucket } from "@aws-cdk/aws-s3";
 
 // Your exported class must be named TargetStack
 export class TargetStack extends Stack {
-    public constructor(scope: App, id: string, props?: StackProps) {
-        super(scope, id, props);
-        // write your code here
-        // access the isPublic property from the JSON file
-        const isPublic = scope.node.tryGetContext("isPublic");
-        new Bucket(this, "MyBucket", {
-            publicReadAccess: isPublic,
-        });
-    }
+	public constructor(scope: App, id: string, props?: StackProps) {
+		super(scope, id, props);
+		// write your code here
+		// access the isPublic property from the JSON file
+		const isPublic = scope.node.tryGetContext("isPublic");
+		new Bucket(this, "MyBucket", {
+			publicReadAccess: isPublic,
+		});
+	}
 }
